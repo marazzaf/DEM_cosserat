b6495b6b5e66747af1a8eb86c3a7faee2cdd6536
Frederic Marazzato <frederic.marazzato@enpc.fr>
2020-09-15 15:31:13 -0500

    Started to clean up the filling of the facet reconstruction matrix.

--- DEM_cosserat/reconstructions.py
+++ DEM_cosserat/reconstructions.py
@@ -6,72 +6,77 @@ from scipy.spatial import ConvexHull, Delaunay, KDTree
 from DEM_cosserat.mesh_related import *
 from DEM_cosserat.errors import *
 
-def DEM_to_DG_matrix(problem,nb_dof_ccG_): #still useful? Not sure
-    """Creates a csr companion matrix to get the cells values of a DEM vector."""
-    nb_cell_dofs = problem.DG_0.dofmap().global_dimension()
-    return sp.eye(nb_cell_dofs, n = nb_dof_ccG_, format='csr')
+#def DEM_to_DG_matrix(problem,nb_dof_ccG_): #still useful? Not sure
+#    """Creates a csr companion matrix to get the cells values of a DEM vector."""
+#    assert isinstance(problem,DEMProblem)
+#    
+#    nb_cell_dofs = problem.DG_0.dofmap().global_dimension()
+#    return sp.eye(nb_cell_dofs, n = nb_dof_ccG_, format='csr')
+#
+#def DEM_to_DG_1_matrix(problem, nb_dof_ccG_, DEM_to_CR):
+#    assert isinstance(problem,DEMProblem)
+#    
+#    EDG_0 = problem.DG_0
+#    EDG_1 = problem.DG_1
+#    tens_DG_0 = problem.W
+#        
+#    dofmap_DG_0 = EDG_0.dofmap()
+#    dofmap_DG_1 = EDG_1.dofmap()
+#    dofmap_tens_DG_0 = tens_DG_0.dofmap()
+#    elt_0 = EDG_0.element()
+#    elt_1 = EDG_1.element()
+#    nb_total_dof_DG_1 = dofmap_DG_1.global_dimension()
+#    nb_dof_grad = dofmap_tens_DG_0.global_dimension()
+#    matrice_resultat_1 = sp.dok_matrix((nb_total_dof_DG_1,nb_dof_ccG_)) #Empty matrix
+#    matrice_resultat_2 = sp.dok_matrix((nb_total_dof_DG_1,nb_dof_grad)) #Empty matrix
+#    
+#    for c in cells(problem.mesh):
+#        index_cell = c.index()
+#        dof_position = dofmap_DG_1.cell_dofs(index_cell)
+#
+#        #filling-in the matrix to have the constant cell value
+#        DG_0_dofs = dofmap_DG_0.cell_dofs(index_cell)
+#        for dof in dof_position:
+#            matrice_resultat_1[dof, DG_0_dofs[dof % problem.d]] = 1.
+#
+#        #filling-in part to add the gradient term
+#        position_barycentre = elt_0.tabulate_dof_coordinates(c)[0]
+#        pos_dof_DG_1 = elt_1.tabulate_dof_coordinates(c)
+#        tens_dof_position = dofmap_tens_DG_0.cell_dofs(index_cell)
+#        for dof,pos in zip(dof_position,pos_dof_DG_1): #loop on quadrature points
+#            diff = pos - position_barycentre
+#            for i in range(problem.dim):
+#                matrice_resultat_2[dof, tens_dof_position[(dof % problem.d)*problem.d + i]] = diff[i]
+#        
+#    return matrice_resultat_1.tocsr() +  matrice_resultat_2.tocsr() * problem.mat_grad * DEM_to_CR
 
-def DEM_to_DG_1_matrix(problem, nb_dof_ccG_, DEM_to_CR):
-    EDG_0 = problem.DG_0
-    EDG_1 = problem.DG_1
-    tens_DG_0 = problem.W
-        
-    dofmap_DG_0 = EDG_0.dofmap()
-    dofmap_DG_1 = EDG_1.dofmap()
-    dofmap_tens_DG_0 = tens_DG_0.dofmap()
-    elt_0 = EDG_0.element()
-    elt_1 = EDG_1.element()
-    nb_total_dof_DG_1 = dofmap_DG_1.global_dimension()
-    nb_dof_grad = dofmap_tens_DG_0.global_dimension()
-    matrice_resultat_1 = sp.dok_matrix((nb_total_dof_DG_1,nb_dof_ccG_)) #Empty matrix
-    matrice_resultat_2 = sp.dok_matrix((nb_total_dof_DG_1,nb_dof_grad)) #Empty matrix
-    
-    for c in cells(problem.mesh):
-        index_cell = c.index()
-        dof_position = dofmap_DG_1.cell_dofs(index_cell)
+#def gradient_matrix(problem):
+#    """Creates a matrix computing the cell-wise gradient from the facet values stored in a Crouzeix-raviart FE vector."""
+#    vol = CellVolume(problem.mesh)
+#
+#    #variational form gradient
+#    u_CR = TrialFunction(problem.U_CR)
+#    Dv_DG = TestFunction(problem.W)
+#    a = inner(grad(u_CR), Dv_DG) / vol * dx
+#    A = assemble(a)
+#    row,col,val = as_backend_type(A).mat().getValuesCSR()
+#    result = sp.csr_matrix((val, col, row))
+#
+#    if problem.dim == 3:
+#        return result,result
+#    elif problem.dim == 2:
+#        u_CR = TrialFunction(problem.PHI_CR)
+#        Dv_DG = TestFunction(problem.PHI_W)
+#        a = inner(grad(u_CR), Dv_DG) / vol * dx
+#        A = assemble(a)
+#        row,col,val = as_backend_type(A).mat().getValuesCSR()
+#        return result,sp.csr_matrix((val, col, row))
+#    else:
+#        raise ValueError('Wrong dimension')
 
-        #filling-in the matrix to have the constant cell value
-        DG_0_dofs = dofmap_DG_0.cell_dofs(index_cell)
-        for dof in dof_position:
-            matrice_resultat_1[dof, DG_0_dofs[dof % problem.d]] = 1.
-
-        #filling-in part to add the gradient term
-        position_barycentre = elt_0.tabulate_dof_coordinates(c)[0]
-        pos_dof_DG_1 = elt_1.tabulate_dof_coordinates(c)
-        tens_dof_position = dofmap_tens_DG_0.cell_dofs(index_cell)
-        for dof,pos in zip(dof_position,pos_dof_DG_1): #loop on quadrature points
-            diff = pos - position_barycentre
-            for i in range(problem.dim):
-                matrice_resultat_2[dof, tens_dof_position[(dof % problem.d)*problem.d + i]] = diff[i]
-        
-    return matrice_resultat_1.tocsr() +  matrice_resultat_2.tocsr() * problem.mat_grad * DEM_to_CR
-
-def gradient_matrix(problem):
-    """Creates a matrix computing the cell-wise gradient from the facet values stored in a Crouzeix-raviart FE vector."""
-    vol = CellVolume(problem.mesh)
-
-    #variational form gradient
-    u_CR = TrialFunction(problem.U_CR)
-    Dv_DG = TestFunction(problem.W)
-    a = inner(grad(u_CR), Dv_DG) / vol * dx
-    A = assemble(a)
-    row,col,val = as_backend_type(A).mat().getValuesCSR()
-    result = sp.csr_matrix((val, col, row))
-
-    if problem.dim == 3:
-        return result,result
-    elif problem.dim == 2:
-        u_CR = TrialFunction(problem.PHI_CR)
-        Dv_DG = TestFunction(problem.PHI_W)
-        a = inner(grad(u_CR), Dv_DG) / vol * dx
-        A = assemble(a)
-        row,col,val = as_backend_type(A).mat().getValuesCSR()
-        return result,sp.csr_matrix((val, col, row))
-    else:
-        raise ValueError('Wrong dimension')
-
-def facet_interpolation(facet_num,pos_bary_cells,pos_vert,pos_bary_facets,dim_,d_, I=10):
+def facet_interpolation(problem, I=10):
     """Computes the reconstruction in the facets of the meh from the dofs of the DEM."""
+    assert isinstance(problem,DEMProblem)
     
     toutes_pos_ddl = [] #ordre : d'abord tous les ddl de cellules puis tous ceux des vertex au bord
     for i in pos_bary_cells.values():
@@ -140,57 +145,32 @@ def facet_interpolation(facet_num,pos_bary_cells,pos_vert,pos_bary_facets,dim_,d
                                 
     return res_num,res_coord
 
-def DEM_to_CR_matrix(problem, nb_dof_ccG, facet_num, vertex_associe_face, num_ddl_vertex, pos_ddl_vertex):
-    dofmap_CR = problem.CR.dofmap()
-    nb_total_dof_CR = dofmap_CR.global_dimension()
+def DEM_to_CR_matrix(problem):
+    assert isinstance(problem,DEMProblem)
 
-    #computing the useful mesh quantities
-    pos_bary_cells = position_cell_dofs(problem.mesh,problem.d)
-    dico_pos_bary_faces = dico_position_bary_face(problem.mesh,problem.d)
+    #dofmaps to fill the matrix
+    dofmap_U_CR = problem.U_CR.dofmap()
+    dofmap_V_CR = problem.V_CR.dofmap()
     
     #Computing the facet reconstructions
-    convex_num,convex_coord = facet_interpolation(facet_num,pos_bary_cells,pos_ddl_vertex,dico_pos_bary_faces,problem.dim,problem.d)
+    convex_num,convex_coord = facet_interpolation(problem)
 
     #Storing the facet reconstructions in a matrix
-    matrice_resultat = sp.dok_matrix((nb_total_dof_CR,nb_dof_ccG)) #Matrice vide.
+    result_matrix = sp.dok_matrix((problem.nb_dof_CR,problem.nb_dof_DEM)) #Empty matrix
     for f in facets(problem.mesh):
-        num_global_face = f.index()
-        num_global_ddl = dofmap_CR.entity_dofs(problem.mesh, problem.dim - 1, array([num_global_face], dtype="uintp"))
-        convexe_f = convex_num.get(num_global_face)
-        convexe_c = convex_coord.get(num_global_face)
+        num_global_facet = f.index()
+        num_global_ddl = dofmap_CR.entity_dofs(problem.mesh, problem.dim - 1, array([num_global_facet], dtype="uintp"))
+        convexe_f = convex_num.get(num_global_facet)
+        convexe_c = convex_coord.get(num_global_facet)
 
-        if convexe_f != None: #Face interne, on interpolle la valeur !
-            for i,j in zip(convexe_f,convexe_c):
-                matrice_resultat[num_global_ddl[0],i[0]] = j
-                if problem.d >= 2:
-                    matrice_resultat[num_global_ddl[1],i[1]] = j
-                if problem.d == 3:
-                    matrice_resultat[num_global_ddl[2],i[2]] = j
-        else: #Face sur le bord, on interpolle la valeur avec les valeurs aux vertex
-            pos_init = vertex_associe_face.get(num_global_face)
-            v1 = num_ddl_vertex[pos_init[0]]
-            v2 = num_ddl_vertex[pos_init[1]]
-            if problem.dim == 2:
-                matrice_resultat[num_global_ddl[0], v1[0]] = 0.5
-                matrice_resultat[num_global_ddl[0], v2[0]] = 0.5
-                if problem.d == 2: #pb vectoriel
-                    matrice_resultat[num_global_ddl[1], v1[1]] = 0.5
-                    matrice_resultat[num_global_ddl[1], v2[1]] = 0.5
-            if problem.dim == 3:
-                v3 = num_ddl_vertex[pos_init[2]]
-                matrice_resultat[num_global_ddl[0], v1[0]] = 1./3.
-                matrice_resultat[num_global_ddl[0], v2[0]] = 1./3.
-                matrice_resultat[num_global_ddl[0], v3[0]] = 1./3.
-                if problem.d >= 2: #deuxième ligne
-                    matrice_resultat[num_global_ddl[1], v1[1]] = 1./3.
-                    matrice_resultat[num_global_ddl[1], v2[1]] = 1./3.
-                    matrice_resultat[num_global_ddl[1], v3[1]] = 1./3.
-                if problem.d == 3: #troisième ligne
-                    matrice_resultat[num_global_ddl[2], v1[2]] = 1./3.
-                    matrice_resultat[num_global_ddl[2], v2[2]] = 1./3.
-                    matrice_resultat[num_global_ddl[2], v3[2]] = 1./3.
+        for i,j in zip(convexe_f,convexe_c):
+            result_matrix[num_global_ddl[0],i[0]] = j
+            if problem.d >= 2:
+                result_matrix[num_global_ddl[1],i[1]] = j
+            if problem.d == 3:
+                result_matrix[num_global_ddl[2],i[2]] = j
         
-    return matrice_resultat.tocsr()
+    return result_matrix.tocsr()
 
 def compute_all_reconstruction_matrices(problem):
     """Computes all the required reconstruction matrices."""
