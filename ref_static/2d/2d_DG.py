5c92302bfa864d7b25d87eac2d71884f21a22b44
Frederic Marazzato <frederic.marazzato@enpc.fr>
2020-10-09 15:19:10 -0500

    Coupling term that was added has been corrected. However, it kills the results. What is wrong with it?

--- ref_static/2d/2d_DG.py
+++ ref_static/2d/2d_DG.py
@@ -136,17 +136,18 @@ v, eta = TestFunctions(V)
 gamma,kappa = strain_bis(u,psi)
 D,D_aux = D_Matrix(G, nu, l, N)
 sigma,mu = stress((gamma,kappa),D_aux)
-epsilon = as_tensor(([0.,-1],[1,0.]))
+aux_1,aux_2 = 1/(1 - N**2),(1 - 2*N**2)/(1 - N**2)
+epsilon = G*(aux_1-aux_2)*as_tensor(([0.,1],[-1,0.]))
 
 hF = CellDiameter(mesh)
 n = FacetNormal(mesh)
     
 elastic = inner(strain(v, eta), D*strain(u, psi))*dx
 inner_pen = penalty_u/hF('+') * inner(jump(u),jump(v)) * dS + penalty_phi/hF('+') * inner(jump(psi),jump(eta)) * dS
-inner_consistency = inner(dot(avg(sigma),n('+')), jump(v))*dS + inner(dot(avg(mu),n('+')), jump(eta))*dS + inner(jump(mu,n), avg(eta))*dS + inner(jump(sigma,n), avg(v))*dS + inner(jump(epsilon*eta,n), avg(u))*dS + inner(dot(avg(epsilon*eta),n('+')), jump(u))*dS
+inner_consistency = inner(dot(avg(sigma),n('+')), jump(v))*dS + inner(dot(avg(mu),n('+')), jump(eta))*dS + inner(jump(mu,n), avg(eta))*dS + inner(jump(sigma,n), avg(v))*dS - inner(jump(epsilon*eta,n), avg(u))*dS - inner(dot(avg(epsilon*eta),n('+')), jump(u))*dS
 bnd_consistency = inner(dot(sigma,n)[1], v[1])*ds(1) + inner(dot(sigma,n)[0], v[0])*ds(2) + inner(dot(mu,n), eta) * (ds(2) + ds(1)) + inner(dot(epsilon*eta,n), u) * (ds(2)+ds(1))
 bnd_pen = penalty_u/hF * u[1] * v[1] * ds(1) + penalty_u/hF * u[0] * v[0] * ds(2) + penalty_phi/hF * inner(psi,eta) * (ds(2) + ds(1))
-a = elastic + inner_pen + bnd_pen + inner_consistency + 2*bnd_consistency
+a = elastic + inner_pen + bnd_pen + inner_consistency# + 2*bnd_consistency
  
 L = inner(t, v)*ds(3) 
 
