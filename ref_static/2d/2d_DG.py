ebfd67466d410dcb59aaea9f6b10f8aedea74716
Frederic Marazzato <frederic.marazzato@enpc.fr>
2020-10-09 14:23:03 -0500

    Looks like there is a problem with the consistency terms... Are there coupled terms???

--- ref_static/2d/2d_DG.py
+++ ref_static/2d/2d_DG.py
@@ -21,7 +21,7 @@ c = l/N
 
 #penalty terms
 penalty_u = 2*G
-penalty_phi = 2*G*l
+penalty_phi = 2*G*l*l
 
 # Convergence
 h = [15, 30, 50, 70, 90] # mesh density
@@ -56,10 +56,10 @@ def D_Matrix(G, nu, l, N):
         [0.0,0.0,0.0,0.0,0.0, 4.0*l**2] ])
     
     d_aux = np.array([ \
-        [(2.0*(1.0 - nu))/(1.0 - 2.0*nu), (2.0*nu)/(1.0 - 2.0 * nu), 0.,0.], \
-        [(2.0*nu)/(1.0 - 2.0*nu), (2.0*(1.0 - nu)) / (1.0 - 2.0*nu), 0.,0.], \
-        [0.,0., 1.0/(1.0 - N**2), (1.0 - 2.0*N**2)/(1.0 - N**2)], \
-        [0.,0., (1.0 - 2.0*N**2)/(1.0 - N**2), 1.0/(1.0 - N**2)] ])
+        [(2*(1 - nu))/(1 - 2*nu), (2*nu)/(1 - 2*nu), 0.,0.], \
+        [(2*nu)/(1 - 2*nu), (2*(1 - nu)) / (1 - 2*nu), 0.,0.], \
+        [0.,0., 1/(1 - N**2), (1 - 2*N**2)/(1 - N**2)], \
+        [0.,0., (1 - 2*N**2)/(1 - N**2), 1/(1 - N**2)] ])
 
     #print(type(d))
     D = G * as_matrix(d)
@@ -78,11 +78,6 @@ def strain(v, eta):
 
     return strain
 
-#def strain_bis(v, omega):
-#    gamma = grad(v) + as_tensor(([0.,-omega],[omega,0.]))
-#    kappa = grad(omega)
-#    return gamma, kappa
-
 def strain_bis(v, eta):
     gamma = as_vector([v[0].dx(0), v[1].dx(1), v[1].dx(0) - eta, v[0].dx(1) + eta])
     kappa = grad(eta)
@@ -95,12 +90,6 @@ def stress(Tuple, D):
     mu = 4*G*l*l * kappa
     return sigma,mu
     
-
-#for hx in h :
-    
-    ## Mesh
-    #geometry = Rectangle(Point(0,0),Point(plate, plate))-Circle(Point(0,0), R, hx)
-    #mesh = generate_mesh(geometry, hx)
 mesh = Mesh()
 with XDMFFile("hole_plate.xdmf") as infile:
     infile.read(mesh)
@@ -139,16 +128,7 @@ left_boundary.mark(boundary_parts, 2)
 top_boundary = TopBoundary()
 top_boundary.mark(boundary_parts, 3)
 
-
-ds = Measure('ds')(subdomain_data=boundary_parts) #Measure("ds")
-
-#u_0 = Constant(0.0)
-#left_U_1 = DirichletBC(U_1, u_0, left_boundary)
-#bot_U_2 = DirichletBC(U_2, u_0, bot_boundary)
-#left_S = DirichletBC(S, u_0, left_boundary)
-#bot_S = DirichletBC(S, u_0, bot_boundary)
-
-#bc = [left_U_1, bot_U_2, left_S, bot_S]
+ds = Measure('ds')(subdomain_data=boundary_parts)
 
 # Variational problem
 u, psi = TrialFunctions(V)
@@ -162,10 +142,10 @@ n = FacetNormal(mesh)
     
 elastic = inner(strain(v, eta), D*strain(u, psi))*dx
 inner_pen = penalty_u/hF('+') * inner(jump(u),jump(v)) * dS + penalty_phi/hF('+') * inner(jump(psi),jump(eta)) * dS
-inner_consistency = inner(dot(avg(sigma),n('+')), jump(v))*dS + inner(jump(sigma,n), avg(v))*dS + inner(dot(avg(mu),n('+')), jump(eta))*dS + inner(jump(mu,n), avg(eta))*dS
+inner_consistency = inner(dot(avg(sigma),n('+')), jump(v))*dS + inner(dot(avg(mu),n('+')), jump(eta))*dS + inner(jump(mu,n), avg(eta))*dS + inner(jump(sigma,n), avg(v))*dS
 bnd_consistency = inner(dot(sigma,n)[1], v[1])*ds(1) + inner(dot(sigma,n)[0], v[0])*ds(2) + inner(dot(mu,n), eta) * (ds(2) + ds(1))
 bnd_pen = penalty_u/hF * u[1] * v[1] * ds(1) + penalty_u/hF * u[0] * v[0] * ds(2) + penalty_phi/hF * inner(psi,eta) * (ds(2) + ds(1))
-a = elastic + inner_pen + bnd_pen + inner_consistency + bnd_consistency
+a = elastic + inner_pen + bnd_pen# + inner_consistency + bnd_consistency
  
 L = inner(t, v)*ds(3) 
 
