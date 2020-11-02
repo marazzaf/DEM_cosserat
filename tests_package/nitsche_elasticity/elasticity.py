ee8c33a27022038d97ff2691973864a162d80ddc
Frederic Marazzato <frederic.marazzato@enpc.fr>
2020-11-02 16:00:22 -0600

    nitsche_penalty.py does not seem to work with manufactured solution. Where is the problem?

--- tests_package/nitsche_elasticity/elasticity.py
+++ tests_package/nitsche_elasticity/elasticity.py
@@ -4,15 +4,17 @@ from dolfin import *
 import matplotlib.pyplot as plt
 
 #Material parameters
-lambda_ = 1
-mu = 2
+nu = 0.3
+E = 70e3
+lambda_ = nu*E / (1+nu) /(1-2*nu)
+mu = 0.5*E/(1+nu)
 
 # Mesh
-nb_elt = 25
+nb_elt = 10
 mesh = UnitSquareMesh(nb_elt, nb_elt)
 
-def boundary(x):
-    return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS or x[1] < DOLFIN_EPS or x[1] > 1.0 - DOLFIN_EPS
+bnd = MeshFunction('size_t', mesh, 1)
+bnd.set_all(0)
 
 U = VectorFunctionSpace(mesh, 'CG', 1)
 #U = VectorFunctionSpace(mesh, 'CR', 1)
@@ -23,10 +25,14 @@ v = TestFunction(U)
 def sigma(u):
     return lambda_*div(u)*Identity(2) + 2*mu*sym(grad(u))
 
-#a = inner(sigma(u), grad(v)) * dx
-a = inner(2*mu*sym(grad(u)), grad(v)) * dx + inner(lambda_*div(u),div(v)) * dx
+#Dirichlet BC
+u_D = Expression(('a*0.5*(x[0]*x[0]+x[1]*x[1])', 'a*0.5*(x[0]*x[0]+x[1]*x[1])'), a=0.8, degree=2)
+bc = DirichletBC(U, u_D, bnd, 0)
 
-b = inner(as_vector((10,5)),v) * dx
+
+a = inner(sigma(u), grad(v)) * dx
+volume_load = Expression(('-a*(lmbda+3*mu)', '-a*(lmbda+3*mu)'), a=0.8, lmbda=lambda_, mu=mu, degree=2)
+b = inner(volume_load,v) * dx
 
 #Nitsche symmetric test
 n = FacetNormal(mesh)
@@ -37,26 +43,45 @@ lhs_nitsche_bis = -inner(dot(2*mu*sym(grad(u)), n), v) * ds - inner(lambda_*div(
 h = CellDiameter(mesh)
 lhs_pen = 2/h * inner(u,v) * ds
 
-#a += lhs_nitsche + lhs_pen #sym pen
+a += lhs_nitsche + lhs_pen #sym pen
 #a += lhs_nitsche #sym no pen
-a += lhs_nitsche_bis #nonsym
+#a += lhs_nitsche_bis #nonsym
 
 sol = Function(U)
 
-#bc = DirichletBC(U, Constant(0), boundary)
-u_D = Constant((1,0))
 rhs_pen = 2/h * inner(u_D,v) * ds
 rhs_nitsche = inner(dot(2*mu*sym(grad(v)), n), u_D) * ds + inner(lambda_*div(v), dot(u_D,n)) * ds
 b += rhs_nitsche #no pen
-#b += -rhs_nitsche + rhs_pen
+b += -rhs_nitsche + rhs_pen #sym CR
+#b -= rhs_nitsche #sym CG
 
 #solve(a == b, sol, bc)
 solve(a == b, sol)
 
+ref = project(u_D, U)
+
 img = plot(sol[0])
+plt.colorbar(img)
+plt.show()
+img = plot(ref[0])
 plt.colorbar(img)
 plt.show()
 
 img = plot(sol[1])
 plt.colorbar(img)
 plt.show()
+img = plot(ref[1])
+plt.colorbar(img)
+plt.show()
+
+#error = sol - ref
+#img = plot(sqrt(error[0]**2 + error[1]**2))
+#plt.colorbar(img)
+#plt.show()
+
+#errors
+err_grad = errornorm(sol, ref, 'H10')
+err_L2 = errornorm(sol, ref, 'L2')
+print(U.dofmap().global_dimension())
+print(err_grad)
+print(err_L2)
