27bd63109e002e7e7aed2627ac4bbc2c74f66cf0
Frederic Marazzato <frederic.marazzato@enpc.fr>
2020-11-02 15:31:48 -0600

    Adding tests of the nitsche pen for elasticity. Wee what happens.

--- tests_package/nitsche_elasticity/elasticity.py
+++ tests_package/nitsche_elasticity/elasticity.py
@@ -4,20 +4,15 @@ from dolfin import *
 import matplotlib.pyplot as plt
 
 #Material parameters
-nu = 0.3
-E = 70e3
-lambda_ = nu*E / (1+nu) /(1-2*nu)
-mu = 0.5*E/(1+nu)
+lambda_ = 1
+mu = 2
 
 # Mesh
-nb_elt = 10
+nb_elt = 25
 mesh = UnitSquareMesh(nb_elt, nb_elt)
 
-#def boundary(x):
+def boundary(x):
     return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS or x[1] < DOLFIN_EPS or x[1] > 1.0 - DOLFIN_EPS
-
-bnd = MeshFunction("size_t", mesh, mesh.topology().dim() - 1)
-bnd.set_all(0)
 
 U = VectorFunctionSpace(mesh, 'CG', 1)
 #U = VectorFunctionSpace(mesh, 'CR', 1)
@@ -28,15 +23,10 @@ v = TestFunction(U)
 def sigma(u):
     return lambda_*div(u)*Identity(2) + 2*mu*sym(grad(u))
 
-#Dirichlet BC
-u_D = Expression(('a*0.5*(x[0]*x[0]+x[1]*x[1])', 'a*0.5*(x[0]*x[0]+x[1]*x[1])'), a=0.8, degree=2)
-bc = DirichletBC(U, u_D, bnd, 0)
+#a = inner(sigma(u), grad(v)) * dx
+a = inner(2*mu*sym(grad(u)), grad(v)) * dx + inner(lambda_*div(u),div(v)) * dx
 
-
-a = inner(sigma(u), grad(v)) * dx
-#a = inner(2*mu*sym(grad(u)), grad(v)) * dx + inner(lambda_*div(u),div(v)) * dx
-volume_load = Expression(('-a*(lambda_+3*mu)', '-a*(lambda_+3*mu)'), a=0.8, degree=2)
-b = inner(volume_load,v) * dx
+b = inner(as_vector((10,5)),v) * dx
 
 #Nitsche symmetric test
 n = FacetNormal(mesh)
@@ -49,17 +39,19 @@ lhs_pen = 2/h * inner(u,v) * ds
 
 #a += lhs_nitsche + lhs_pen #sym pen
 #a += lhs_nitsche #sym no pen
-#a += lhs_nitsche_bis #nonsym
+a += lhs_nitsche_bis #nonsym
 
 sol = Function(U)
 
+#bc = DirichletBC(U, Constant(0), boundary)
+u_D = Constant((1,0))
 rhs_pen = 2/h * inner(u_D,v) * ds
 rhs_nitsche = inner(dot(2*mu*sym(grad(v)), n), u_D) * ds + inner(lambda_*div(v), dot(u_D,n)) * ds
-#b += rhs_nitsche #no pen
+b += rhs_nitsche #no pen
 #b += -rhs_nitsche + rhs_pen
 
-solve(a == b, sol, bc)
-#solve(a == b, sol)
+#solve(a == b, sol, bc)
+solve(a == b, sol)
 
 img = plot(sol[0])
 plt.colorbar(img)
