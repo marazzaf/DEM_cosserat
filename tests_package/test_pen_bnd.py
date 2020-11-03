82973267252054d2227abea5c2c0cab0c1b5eefb
marazzaf <frederic.marazzato@ponts.org>
2020-11-03 09:55:00 -0600

    test_pen_bnd is finally a real unit test. Prepare a 3d version for afterwards.

--- tests_package/test_pen_bnd.py
+++ tests_package/test_pen_bnd.py
@@ -9,6 +9,7 @@ sys.path.append('../')
 from DEM_cosserat.DEM import *
 from DEM_cosserat.miscellaneous import *
 from scipy.sparse.linalg import spsolve
+import pytest #for unit tests
 
 # Parameters
 nu = 0.3 # Poisson's ratio
@@ -24,47 +25,50 @@ d = (1-2*N*N)/(1-N*N)
     
 # Mesh
 L = 0.5
-nb_elt = 25
-mesh = RectangleMesh(Point(-L,-L),Point(L,L),nb_elt,nb_elt,"crossed")
+nb_elt = 10
+#mesh = RectangleMesh(Point(-L,-L),Point(L,L),nb_elt,nb_elt,"crossed")
+@pytest.mark.parametrize("mesh", [RectangleMesh(Point(-L,-L),Point(L,L),nb_elt,nb_elt,"crossed")]) #, BoxMesh(Point(-L, -L, -L), Point(L, L, L), nb_elt, nb_elt, nb_elt)])
 
-#Creating the DEM problem
-problem = DEMProblem(mesh, 2*G, 2*G*l*l) #sure about second penalty term? #2*G*l #22*G*l*l ?
-boundary_parts = MeshFunction("size_t", mesh, mesh.topology().dim() - 1)
+def test_pen_bnd(mesh):
 
-#compliance tensor
-problem.D = problem.D_Matrix(G, nu, N, l)
+    #Creating the DEM problem
+    problem = DEMProblem(mesh, 2*G, 2*G*l*l)
+    boundary_parts = MeshFunction("size_t", mesh, mesh.topology().dim() - 1)
 
-# Variational problem
-A = problem.elastic_bilinear_form() #, strain, stresses)
+    #compliance tensor
+    problem.D = problem.D_Matrix(G, nu, N, l)
 
-#Penalty matrix
-A += inner_penalty(problem)
+    # Variational problem
+    A = problem.elastic_bilinear_form()
 
-#rhs
-t = Constant((-(a+b),-(a+b),0))
-rhs = problem.assemble_volume_load(t)
+    #Penalty matrix
+    A += inner_penalty(problem)
 
-#Nitsche penalty bilinear form. Homogeneous Dirichlet in this case.
-#A += lhs_nitsche_penalty(problem, strain, stresses)
-bc = [[0, Constant(0)], [1, Constant(0)], [2, Constant(0)]]
-#bc = [[0, Constant(1)], [1, Constant(1)], [2, Constant(1)]]
-A += lhs_bnd_penalty(problem, boundary_parts, bc)
-rhs += rhs_bnd_penalty(problem, boundary_parts, bc)
-#A += lhs_bnd_penalty(problem)
+    #rhs
+    t = Constant((-(a+b),-(a+b),0))
+    rhs = problem.assemble_volume_load(t)
 
-#Solving linear problem
-v = spsolve(A,rhs)
-v_h = Function(problem.V_DG1)
-v_h.vector().set_local(problem.DEM_to_DG1 * v)
-u_h, phi_h = v_h.split()
+    #Nitsche penalty bilinear form. Homogeneous Dirichlet in this case.
+    #A += lhs_nitsche_penalty(problem, strain, stresses)
+    bc = [[0, Constant(0)], [1, Constant(0)], [2, Constant(0)]]
+    #bc = [[0, Constant(1)], [1, Constant(1)], [2, Constant(1)]]
+    A += lhs_bnd_penalty(problem, boundary_parts, bc)
+    rhs += rhs_bnd_penalty(problem, boundary_parts, bc)
+    #A += lhs_bnd_penalty(problem)
 
-F = FacetArea(problem.mesh)
-W = FunctionSpace(problem.mesh, 'CR', 1)
-w = TestFunction(W)
-aire = sum(assemble(w * ds).get_local())
-print(assemble(u_h[0] / aire * ds))
-print(assemble(u_h[1] / aire * ds))
-print(assemble(phi_h / aire * ds))
+    #Solving linear problem
+    v = spsolve(A,rhs)
+    v_h = Function(problem.V_DG1)
+    v_h.vector().set_local(problem.DEM_to_DG1 * v)
+    u_h, phi_h = v_h.split()
+
+    F = FacetArea(problem.mesh)
+    W = FunctionSpace(problem.mesh, 'CR', 1)
+    w = TestFunction(W)
+    aire = sum(assemble(w * ds).get_local())
+    assert round(assemble(u_h[0] / aire * ds), 2) == 0
+    assert round(assemble(u_h[1] / aire * ds), 2) == 0
+    assert round(assemble(phi_h / aire * ds), 2) == 0
 
 ##Loading ref
 #ref_mesh = HDF5File(MPI.comm_world, 'ref_mesh_test_pen.hdf5', 'r')
@@ -98,16 +102,16 @@ print(assemble(phi_h / aire * ds))
 #plt.show()
 #sys.exit()
 
-fig = plot(u_h[0])
-plt.colorbar(fig)
-#plt.savefig('u_x_25.pdf')
-plt.show()
-fig = plot(u_h[1])
-plt.colorbar(fig)
-#plt.savefig('u_y_25.pdf')
-plt.show()
-fig = plot(phi_h)
-plt.colorbar(fig)
-#plt.savefig('phi_25.pdf')
-plt.show()
-sys.exit()
+#fig = plot(u_h[0])
+#plt.colorbar(fig)
+##plt.savefig('u_x_25.pdf')
+#plt.show()
+#fig = plot(u_h[1])
+#plt.colorbar(fig)
+##plt.savefig('u_y_25.pdf')
+#plt.show()
+#fig = plot(phi_h)
+#plt.colorbar(fig)
+##plt.savefig('phi_25.pdf')
+#plt.show()
+#sys.exit()
