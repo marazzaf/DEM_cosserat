070335c6a1d53c2448f2a90c6c8cd32c10e4afd0
marazzaf <frederic.marazzato@ponts.org>
2020-11-10 13:01:19 -0600

    Non-symmetric Nitsche penalty gives the best results (but still not the right convergence orders) Why?

--- tests_package/convergence.py
+++ tests_package/convergence.py
@@ -5,9 +5,9 @@ import numpy as np
 name = 'convergence_DEM_manufactured.txt'
 res = np.loadtxt(name)
 
-orders_energy = 2 * np.log(res[1:,3] / res[:-1,3]) / np.log(res[:-1, 0] / res[1:,0]) #3 * ???
-orders_grad = 2 * np.log(res[1:,1] / res[:-1,1]) / np.log(res[:-1, 0] / res[1:,0])
-orders_l2 = 2 * np.log(res[1:,2] / res[:-1,2]) / np.log(res[:-1, 0] / res[1:,0])
+orders_energy = 3 * np.log(res[1:,3] / res[:-1,3]) / np.log(res[:-1, 0] / res[1:,0]) #3 * ???
+orders_grad = 3 * np.log(res[1:,1] / res[:-1,1]) / np.log(res[:-1, 0] / res[1:,0])
+orders_l2 = 3 * np.log(res[1:,2] / res[:-1,2]) / np.log(res[:-1, 0] / res[1:,0])
 
 print(orders_energy)
 print(orders_grad)
