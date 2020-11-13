Corrected the computation of the errors in energy norm. However, the convergence rates are still not the right ones. Write the test to see if errors comes from LS penalty on boundary!

tests_package/manufactured_solution.py
@@ -24,12 +24,12 @@ d = (1-2*N*N)/(1-N*N)
     
 # Mesh
 L = 0.5
-nb_elt = 40
+nb_elt = 80
 mesh = RectangleMesh(Point(-L,-L),Point(L,L),nb_elt,nb_elt,"crossed")
 
 #Creating the DEM problem
-problem = DEMProblem(mesh, 2*G, 2*G*l*l) #sure about second penalty term?
-#problem = DEMProblem(mesh, 4*G, 4*G*l*l)
+#problem = DEMProblem(mesh, 2*G, 2*G*l*l) #sure about second penalty term?
+problem = DEMProblem(mesh, 4*G, 4*G*l*l)
 
 boundary_parts = MeshFunction("size_t", mesh, mesh.topology().dim() - 1)
 boundary_parts.set_all(0)
@@ -47,8 +47,8 @@ elas = problem.elastic_bilinear_form()
 lhs = elas
 
 #Penalty matrix
-inner = inner_penalty_light(problem) #light
-lhs += inner
+inner_pen = inner_penalty_light(problem) #light
+lhs += inner_pen
 
 #rhs
 t = Expression(('-G*(2*A*(a+c)+B*(d-c))','-G*(2*A*(a+c)+B*(d-c))','-2*(x[0]-x[1] )*(d-c)*(B-A)*G'), G=G, A=A, B=B, a=a, b=b, c=c, d=d, degree = 1)
@@ -107,60 +107,78 @@ phi = interpolate(phi_D, U)
 #sys.exit()
 
 
-fig = plot(u_h[0])
-plt.colorbar(fig)
-##plt.savefig('u_x_25.pdf')
-plt.show()
-fig = plot(u[0])
-plt.colorbar(fig)
-##plt.savefig('ref_u_x_25.pdf')
-plt.show()
-#fig = plot(u_h[0]-u[0])
+#fig = plot(u_h[0])
 #plt.colorbar(fig)
+###plt.savefig('u_x_25.pdf')
 #plt.show()
-#
-fig = plot(u_h[1])
-plt.colorbar(fig)
-##plt.savefig('u_y_25.pdf')
-plt.show()
-fig = plot(u[1])
-plt.colorbar(fig)
-##plt.savefig('ref_u_y_25.pdf')
-plt.show()
-#fig = plot(u_h[1]-u[1])
+#fig = plot(u[0])
 #plt.colorbar(fig)
+###plt.savefig('ref_u_x_25.pdf')
 #plt.show()
-#
-fig = plot(phi_h)
-plt.colorbar(fig)
-##plt.savefig('phi_25.pdf')
-plt.show()
-fig = plot(phi)
-plt.colorbar(fig)
-##plt.savefig('ref_phi_25.pdf')
-plt.show()
-#fig = plot(phi_h-phi)
+##fig = plot(u_h[0]-u[0])
+##plt.colorbar(fig)
+##plt.show()
+##
+#fig = plot(u_h[1])
 #plt.colorbar(fig)
+###plt.savefig('u_y_25.pdf')
 #plt.show()
-#sys.exit()
+#fig = plot(u[1])
+#plt.colorbar(fig)
+###plt.savefig('ref_u_y_25.pdf')
+#plt.show()
+##fig = plot(u_h[1]-u[1])
+##plt.colorbar(fig)
+##plt.show()
+##
+#fig = plot(phi_h)
+#plt.colorbar(fig)
+###plt.savefig('phi_25.pdf')
+#plt.show()
+#fig = plot(phi)
+#plt.colorbar(fig)
+###plt.savefig('ref_phi_25.pdf')
+#plt.show()
+##fig = plot(phi_h-phi)
+##plt.colorbar(fig)
+##plt.show()
+##sys.exit()
 
-#write convergence test to see if okay...
+#DG1 errors
 err_grad = np.sqrt(errornorm(u_h, u, 'H10')**2 + errornorm(phi_h, phi, 'H10')**2)
-error = v - project(as_vector((u[0],u[1],phi)), problem.V_DG).vector().get_local()
-err_energy = np.sqrt(0.5 * np.dot(error, lhs*error)) #lhs #elas #inner #bnd
 err_L2 = np.sqrt(errornorm(u_h, u, 'L2')**2 + errornorm(phi_h, phi, 'L2')**2)
 print(problem.nb_dof_DEM)
 print(err_grad)
 print(err_L2)
-print('Errors in energy:')
+
+#DG0 L2 error
+v_h = Function(problem.V_DG)
+v_h.vector().set_local(v)
+u_h, phi_h = v_h.split()
+U = VectorFunctionSpace(problem.mesh, 'DG', 0)
+ref_u = interpolate(u_D, U)
+U = FunctionSpace(problem.mesh, 'DG', 0)
+ref_phi = interpolate(phi_D, U)
+err_L2 = np.sqrt(errornorm(u_h, ref_u, 'L2')**2 + errornorm(phi_h, ref_phi, 'L2')**2)
+print(err_L2)
+
+#print('Errors in energy:')
 #print(err_energy)
-print('Tot: %.2f' % (np.sqrt(0.5 * np.dot(error, lhs*error))))
+#print('Tot: %.2f' % (np.sqrt(0.5 * np.dot(error, lhs*error))))
 #print('No bnd energy: %.2f' % (np.sqrt(0.5 * np.dot(error, (inner+elas)*error))))
-print('Pen: %.2f' % (np.sqrt(0.5 * np.dot(error, (inner+bnd)*error))))
-print('Elas: %.2f' % (np.sqrt(0.5 * np.dot(error, elas*error))))
-print('Bnd pen: %.2f' % (np.sqrt(0.5 * np.dot(error, bnd*error))))
-print('Bnd pen squared: %.2f' % (np.dot(error, bnd*error)))
+#print('Pen: %.2f' % (np.sqrt(0.5 * np.dot(error, (inner+bnd)*error))))
+#print('Elas: %.2f' % (np.sqrt(0.5 * np.dot(error, elas*error))))
+#print('Bnd pen: %.2f' % (np.sqrt(0.5 * np.dot(error, bnd*error))))
+#print('Bnd pen squared: %.2f' % (np.dot(error, bnd*error)))
 
 #print(errornorm(u_h, u, 'L2'))
 #print(errornorm(phi_h, phi, 'L2'))
+
+#For energy error
+Mat = energy_error_matrix(problem, boundary_parts)
+
+#Energy error
+error = v - project(as_vector((ref_u[0],ref_u[1],ref_phi)), problem.V_DG).vector().get_local()
+en_error = np.dot(error, Mat*error)
+print('Error in energy norm: %.5e' % (np.sqrt(en_error)))
 