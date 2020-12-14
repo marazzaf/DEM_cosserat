16f3253bdc72ce4dc37056829cd8b5aea32437a3
Frederic Marazzato <marazzato@lsu.edu>
2020-12-14 14:23:28 -0600

    There was a problem in convergence.py. Check everything to be sure about the computed convergence rates. Also, cehck why convergence in L2 norm and energy norm are at the same rate...

--- manufactured/convergence.py
+++ manufactured/convergence.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 
 import numpy as np
+from scipy.stats import linregress
 
 name = 'convergence_DEM_manufactured.txt'
 #name = 'conv_bis.txt'
@@ -12,9 +13,15 @@ orders_energy = 2 * np.log(res[:-1,3] / res[1:,3]) / np.log(res[1:, 0] / res[:-1
 orders_l2 = 2 * np.log(res[:-1,1] / res[1:,1]) / np.log(res[1:, 0] / res[:-1,0])
 orders_grad = 2 * np.log(res[:-1,2] / res[1:,2]) / np.log(res[1:, 0] / res[:-1,0])
 
-print(orders_energy)
-print(orders_grad)
-print(orders_l2)
+print(1/orders_l2) #1
+print(1/orders_grad) #2
+print(np.mean(1/orders_l2))
+print(1/orders_energy) #3
+
+#Linear regression
+slope, intercept, r_value, p_value, std_err = linregress(np.log(res[:,1]), -np.log(res[:,0]))
+print(slope/2)
+print(r_value*r_value)
 
 #test interpolation
 #orders_l2_u = 2 * np.log(res[:-1,1] / res[1:,1]) / np.log(res[1:,0] / res[:-1,0])
