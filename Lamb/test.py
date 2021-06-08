6792956b092581c4dff55e30e17d24231d6c8931
Frederic Marazzato <marazzato@lsu.edu>
2021-06-08 17:49:43 -0500

    Check if dynamics is ok. Does not look that way.

--- Lamb/test.py
+++ Lamb/test.py
@@ -13,7 +13,7 @@ from ufl import sign
     
 # Mesh
 Lx,Ly = 4e3,2e3
-nb_elt = 5 #50 computation #5 #debug
+nb_elt = 50 #50 computation #5 #debug
 mesh = RectangleMesh(Point(-Lx/2,0),Point(Lx/2,Ly),int(Lx/Ly)*nb_elt,nb_elt,"crossed")
 
 # Parameters
@@ -40,7 +40,7 @@ sigma = 14.5
 r_squared = Expression('pow(x[0]-x0,2) + pow(x[1]-y0,2)', x0=x0, y0=y0, degree=2)
 #psi = 1e10 * (1 - 0.5 * r_squared/sigma**2) * exp(-0.5*r_squared/sigma**2) / (np.pi*sigma**4)
 #psi = Expression('pow(x[0]-x0,2)+pow(x[1]-y0,2) < 1 ? (1 - t*t/sigma/sigma) * exp(-0.5*t*t/sigma/sigma) : 0', x0=x0, y0=y0, sigma=sigma, t=0, degree = 1)
-t = 0
+t = 0.5
 psi = (1 - t*t/sigma/sigma) * exp(-0.5*t*t/sigma/sigma)
 x = SpatialCoordinate(mesh)
 X = (x[1]-y0) / (x[0]-x0)
@@ -88,16 +88,20 @@ a_old = Function(problem.V_DG)
 #Necessary
 v_DG,psi_DG = TestFunctions(problem.V_DG)
 
+#test
+truc = Expression(('pow(x[0]-x0,2)+pow(x[1]-y0,2) < 1e3 ? 1 : 0', '0', '0'), x0=x0, y0=y0, degree = 1)
+v_old = interpolate(truc, problem.V_DG)
+
 #Mass matrix
 I = 2/5*l*l
 M = mass_matrix(problem, rho, I)
 K += M/beta/dt_**2
 K = PETScMatrix(K)
 #corresponding rhs
-def L():
-    truc = (u_old+dt_*v_old)/beta/dt_**2 + (1-2*beta)/2/beta*a_old
-    disp = as_vector((truc[0],truc[1]))
-    rot = truc[2]
+def L(uu, vv, aa):
+    aux = (uu+dt_*vv)/beta/dt_**2 + (1-2*beta)/2/beta*aa
+    disp = as_vector((aux[0],aux[1]))
+    rot = aux[2]
     return rho*inner(disp, v_DG)*dx + rho*I*inner(rot,psi_DG)*dx
 
 #outputs
@@ -107,12 +111,13 @@ file = File(folder+"/output.pvd")
 for (i, dt) in enumerate(np.diff(time)):
     t = time[i+1]
     print("Time: ", t)
-    psi = (1 - t*t/sigma/sigma) * np.exp(-0.5*t*t/sigma/sigma) / r_squared
+    #psi = (1 - t*t/sigma/sigma) * np.exp(-0.5*t*t/sigma/sigma) / r_squared
     load = psi * as_vector((cos_theta, sin_theta, 0)) # * psi
 
     # Solve for new displacement
     Rhs = problem.assemble_volume_load(load)
-    res = PETScVector(Rhs) + as_backend_type(assemble(L()))
+    res = as_backend_type(assemble(L(u_old, v_old, a_old))) + PETScVector(Rhs)
+    #res = PETScVector(Rhs)
     solve(K, u.vector(), res, 'mumps')
     
     # Update old fields with new quantities
@@ -121,13 +126,15 @@ for (i, dt) in enumerate(np.diff(time)):
     a_old.vector()[:] = (u_vec-u0_vec-dt_*v0_vec)/beta/dt_**2 - (1-2*beta)/2/beta*a0_vec
     v_old.vector()[:] = v0_vec + dt_*((1-gamma)*a0_vec + gamma*a_old.vector())
 
-    img = plot(v_old[1])
+    img = plot(sqrt(u[1]*u[1]+u[0]*u[0]))
+    #img = plot(v_old[1])
     plt.colorbar(img)
     plt.show()
     #sys.exit()
 
-    #output
-    file.write(u, t)
-    file.write(v_old, t)
+    ##output
+    #file.write(u, t)
+    #file.write(v_old, t)
+    #u_old.vector()[:] = u.vector() #Useful?
 
 
