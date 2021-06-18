3269f26eaa7563b6dfdc5cdd5494f43ded7a7454
marazzaf <frederic.marazzato@ponts.org>
2021-06-18 09:25:16 -0500

    Debug the Crank-Nicholson in DEM! Also, add damping to impose no velocity on bottom part.

--- Lamb/DEM.py
+++ Lamb/DEM.py
@@ -16,9 +16,10 @@ parameters["form_compiler"]["cpp_optimize"] = True
 parameters["form_compiler"]["optimize"] = True
     
 # Mesh
-Lx,Ly = 4e3,2e3
-nb_elt = 100 #100 computation #5 #debug
+Lx,Ly = 2e3,1e3
+nb_elt = 10 #100 computation #5 #debug
 mesh = RectangleMesh(Point(-Lx/2,0),Point(Lx/2,Ly),int(Lx/Ly)*nb_elt,nb_elt,"crossed")
+folder = 'test' #'big' #'test'
 
 # Parameters
 nu = 0.25 # Poisson's ratio
@@ -41,16 +42,21 @@ boundary_parts.set_all(0)
 def top(x, on_boundary):
     return near(x[1], Ly) and on_boundary
 
+def down(x, on_boundary):
+    return near(x[1], 0) and on_boundary
+
 top_boundary = AutoSubDomain(top)
 top_boundary.mark(boundary_parts, 1)
-
+down_boundary = AutoSubDomain(down)
+down_boundary.mark(boundary_parts, 2)
 ds = ds(subdomain_data=boundary_parts)
 
 #volume load
 x0 = 0
-y0 = Ly - 20
+y0 = Ly - 100
 sigma = 14.5
-domain = Expression('pow(x[0]-x0,2) + pow(x[1]-y0,2) < h*h ? 1 : 0', h=h, x0=x0, y0=y0, degree=2)
+radius = 50
+domain = Expression('sqrt(pow(x[0]-x0,2) + pow(x[1]-y0,2)) < radius ? 1 : 0', radius=radius, x0=x0, y0=y0, degree=2)
 psi = Expression('2/sqrt(3*sigma)/pow(pi,0.25)*(1 - t*t/sigma/sigma) * exp(-0.5*t*t/sigma/sigma)', sigma=sigma, t=0, degree = 1)
 load = psi * domain * Constant((0,-1,0))
 Rhs = problem.assemble_volume_load(load)
@@ -66,7 +72,7 @@ K = elas
 K += inner_penalty(problem)
 
 #Nitsche penalty bilinear form
-bc = [[0, Constant(0), 0], [1, Constant(0), 0], [2, Constant(0), 0]] #Homogeneous Dirichlet on all boundary apart from top surface
+bc = [[0, Constant(0), 2], [1, Constant(0), 2], [2, Constant(0), 2]] #Homogeneous Dirichlet on all boundary apart from top surface
 K += lhs_bnd_penalty(problem, boundary_parts, bc)
 
 #Newmark-beta parameters
@@ -84,15 +90,17 @@ a_old = Function(problem.V_DG)
 #Necessary
 v_DG,psi_DG = TestFunctions(problem.V_DG)
 
-#test
-truc = Expression(('x[0] < 0 ? 1 : 0', '0', '0'), x0=x0, Ly=Ly, degree = 1)
-#v_old = interpolate(truc, problem.V_DG)
+# Time-stepping implicit
+T = 0.5 #second
+Nsteps = 100
+dt_ = T/Nsteps
+time = np.linspace(0, T, Nsteps+1)
 
 #Mass matrix
 I = 2/5*l*l
 M = mass_matrix(problem, rho, I)
-#K += M/beta/dt_**2
-#K = PETScMatrix(K)
+K += M/beta/dt_**2
+K = PETScMatrix(K)
 #corresponding rhs
 def L(uu, vv, aa):
     aux = (uu+dt_*vv)/beta/dt_**2 + (1-2*beta)/2/beta*aa
@@ -101,79 +109,68 @@ def L(uu, vv, aa):
     return rho*inner(disp, v_DG)*dx + rho*I*inner(rot,psi_DG)*dx
 
 #outputs
-folder = 'big' #'big' #'test'
 file = XDMFFile(folder+"/output.xdmf")
 file.parameters["flush_output"] = True
 file.parameters["functions_share_mesh"] = True
 file.parameters["rewrite_function_mesh"] = False
 
-# Time-stepping implicit
-T = 1 #second
-#Nsteps = 50
-##dt_ = T/Nsteps
-##time = np.linspace(0, T, Nsteps+1)
-##test
-#dt_ = 1e-10
-#time = np.linspace(0, 50*dt_, Nsteps+1)
-
-#test Verlet
-M = mass_matrix_vec(problem, rho, I).vec()
-#M_min = min(PETScVector(M))
-#eigensolver = SLEPcEigenSolver(PETScMatrix(K))
-#eigensolver.solve()
-#print(eigensolver.get_number_converged())
-#K_max, i = eigensolver.get_eigenvalue(0)
-#print(K_max)
-#dt_ = np.sqrt(M_min/K_max)
-#print(dt_)
-dt_ = 1e-6
-Nsteps = int(T/dt_)
-time = np.linspace(0, T, Nsteps+1)
-for (i, dt) in enumerate(np.diff(time)):
-    t = time[i+1]
-    if i%100 == 0:
-        print("Time: ", t)
-    psi.t = t 
-
-    Rhs = problem.assemble_volume_load(load)
-
-    u.vector()[:] += v_old.vector() * dt_
-    v_old.vector()[:] += dt_ * (Rhs - K*u.vector().vec()) / M
-    #output
-    if i%100 == 0:
-        u_DG1.vector()[:] = problem.DEM_to_DG1 * u.vector().vec()
-        v_old_DG1.vector()[:] = problem.DEM_to_DG1 * v_old.vector().vec()
-        file.write(u_DG1, t)
-        file.write(v_old_DG1, t)
-
-##implicit integration
+##test Verlet
+#M = mass_matrix_vec(problem, rho, I).vec()
+##M_min = min(PETScVector(M))
+##eigensolver = SLEPcEigenSolver(PETScMatrix(K))
+##eigensolver.solve()
+##print(eigensolver.get_number_converged())
+##K_max, i = eigensolver.get_eigenvalue(0)
+##print(K_max)
+##dt_ = np.sqrt(M_min/K_max)
+##print(dt_)
+#dt_ = 1e-6
+#Nsteps = int(T/dt_)
+#time = np.linspace(0, T, Nsteps+1)
 #for (i, dt) in enumerate(np.diff(time)):
 #    t = time[i+1]
-#    print("Time: ", t)
-#    psi.t = t
-#    #load = psi * Constant((0,-1,0))
+#    if i%100 == 0:
+#        print("Time: ", t)
+#    psi.t = t 
 #
-#    # Solve for new displacement
 #    Rhs = problem.assemble_volume_load(load)
-#    res = as_backend_type(assemble(L(u_old, v_old, a_old))) + PETScVector(Rhs)
-#    res = as_backend_type(assemble(L(u_old, v_old, a_old)))
-#    solve(K, u.vector(), res, 'mumps')
-#    
-#    # Update old fields with new quantities
-#    u_vec, u0_vec  = u.vector(), u_old.vector()
-#    v0_vec, a0_vec = v_old.vector(), a_old.vector()
-#    a_old.vector()[:] = (u_vec-u0_vec-dt_*v0_vec)/beta/dt_**2 - (1-2*beta)/2/beta*a0_vec
-#    v_old.vector()[:] = v0_vec + dt_*((1-gamma)*a0_vec + gamma*a_old.vector())
-#    u_old.vector()[:] = u.vector() #Useful?
 #
-#    #img = plot(sqrt(u[1]*u[1]+u[0]*u[0]))
-#    img = plot(sqrt(v_old[1]*v_old[1]+v_old[0]*v_old[0]))
-#    plt.colorbar(img)
-#    plt.show()
-#    #sys.exit()
-#
+#    u.vector()[:] += v_old.vector() * dt_
+#    v_old.vector()[:] += dt_ * (Rhs - K*u.vector().vec()) / M
 #    #output
-#    file.write(u, t)
-#    file.write(v_old, t)
+#    if i%100 == 0:
+#        u_DG1.vector()[:] = problem.DEM_to_DG1 * u.vector().vec()
+#        v_old_DG1.vector()[:] = problem.DEM_to_DG1 * v_old.vector().vec()
+#        file.write(u_DG1, t)
+#        file.write(v_old_DG1, t)
+
+#implicit integration
+for (i, dt) in enumerate(np.diff(time)):
+    t = time[i+1]
+    print("Time: ", t)
+    psi.t = t
+    #load = psi * Constant((0,-1,0))
+
+    # Solve for new displacement
+    Rhs = problem.assemble_volume_load(load)
+    res = as_backend_type(assemble(L(u_old, v_old, a_old))) + PETScVector(Rhs)
+    solve(K, u.vector(), res, 'mumps')
+    
+    # Update old fields with new quantities
+    u_vec, u0_vec  = u.vector(), u_old.vector()
+    v0_vec, a0_vec = v_old.vector(), a_old.vector()
+    a_old.vector()[:] = (u_vec-u0_vec-dt_*v0_vec)/beta/dt_**2 - (1-2*beta)/2/beta*a0_vec
+    v_old.vector()[:] = v0_vec + dt_*((1-gamma)*a0_vec + gamma*a_old.vector())
+    u_old.vector()[:] = u.vector() #Useful?
+
+    ##img = plot(sqrt(u[1]*u[1]+u[0]*u[0]))
+    #img = plot(sqrt(v_old[1]*v_old[1]+v_old[0]*v_old[0]))
+    #plt.colorbar(img)
+    #plt.show()
+    ##sys.exit()
+
+    #output
+    file.write(u, t)
+    file.write(v_old, t)
 
 
